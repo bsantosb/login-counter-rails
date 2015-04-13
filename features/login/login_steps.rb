f27129f81896b4c13851190e71a915cb9fab66bd
Beatriz Santos <bsantosb@snu.ac.kr>
2015-04-13 21:01:23 +0900

    cucumber session done

--- features/login/login_steps.rb
+++ features/login/login_steps.rb
@@ -3,3 +3,9 @@ user = User.new({username: name, password: pwd, counter:
 1})
 user.save
 end
+
+When /^I login up as "(\w+)" with "(\w+)"$/ do |name, pwd|
+fill_in('username_input', :with => name)
+fill_in('password_input', :with => pwd)
+click_button('login_btn')
+end
